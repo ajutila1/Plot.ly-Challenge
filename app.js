4c32f433a001fa1d286e0a595b034adcffc6d945
ajutila1 <64799109+ajutila1@users.noreply.github.com>
2020-09-21 13:32:43 -0400

    ID selector

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-d3.json("data/samples.json").then((importedData) => {
-    console.log(importedData);
-})
+// d3.json("data/samples.json").then((importedData) => {
+    // console.log(importedData);
+// })
+
+function optionChanged(id) {
+    graphs(id);
+}
+
+function init() {
+
+    var dataSelect = d3.select("#selDataset");
+
+    d3.json("data/samples.json").then((importedData) => {
+        console.log(importedData);
+
+        importedData.names.forEach(function(name) {
+            dataSelect.append("option").text(name).property("value");
+        });
+
+        graphs(importedData.names[0]);
+        
+    });
+}
+
+init();
